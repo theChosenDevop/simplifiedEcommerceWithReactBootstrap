c6e39c57b1537f244042c7a711da7ec0fc198e60
Oluwatobi Adesanya <104910188+theChosenDevop@users.noreply.github.com>
2022-10-05 11:57:14 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {useThemeHook} from './GlobalComponents/ThemeProvider'
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useThemeHook } from './GlobalComponents/ThemeProvider';
 import Header from './Components/Header'
+import { Router } from "@reach/router";
 
-const App = () => {
-  const [theme] = useThemeHook()
+//Pages
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
 
+
+function App() {
+  const [theme] = useThemeHook();
   return (
-    <main className={theme ? 'bg-black': 'bg-light-2'}><Header /></main>
-  )
+    <main className={theme? 'bg-black': 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
+      <Header/>
+      <Router>
+        <Home path="/" />
+        <Cart path="/cart" />
+      </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
